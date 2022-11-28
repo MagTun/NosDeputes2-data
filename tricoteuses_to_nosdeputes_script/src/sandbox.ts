a8c9a0dbe6d2252713d3984bcd410adb0b3743cd
implicitdef <implicitdef@users.noreply.github.com>
2022-11-28 20:06:43 +0100

    WIP

--- tricoteuses_to_nosdeputes_script/src/sandbox.ts
+++ tricoteuses_to_nosdeputes_script/src/sandbox.ts
@@ -1,24 +1,32 @@
-import path from 'path'
 import fs from 'fs'
 import { CliArgs } from './utils/cli'
-import { AM030 } from './utils/datasets'
-
+import slugify from 'slugify'
 export function sandbox(args: CliArgs) {
   const { workdir } = args
 
-  const subdir = path.join(workdir, AM030, 'acteurs')
-  const files = readFilesInSubdir(subdir)
-  for (const f of files) {
-    const json = readFileAsJson(path.join(subdir, f))
-    json.mandats.forEach((mandat: any) => {
-      const { organesRefs } = mandat
-      if ((organesRefs.length = 1)) {
-        console.log(
-          `acteur ${json.uid} has mandat ${mandat.uid} ${mandat.xsiType} with organeRefs ${organesRefs}`,
-        )
-      }
-    })
+  for (const depute of readAllDeputesJsonFromNosdeputes()) {
+    const oldSlug = depute.slug
+    slugify.extend({ "'": '-', '/': '-' })
+    const mySlug = slugify(depute.nom, { lower: true, locale: 'fr' })
+
+    if (oldSlug !== mySlug) {
+      console.log('Problem with this one', depute.nom, oldSlug, '!=', mySlug)
+    }
   }
+
+  // const subdir = path.join(workdir, AM030, 'acteurs')
+  // const files = readFilesInSubdir(subdir)
+  // for (const f of files) {
+  //   const json = readFileAsJson(path.join(subdir, f))
+  //   json.mandats.forEach((mandat: any) => {
+  //     const { organesRefs } = mandat
+  //     if ((organesRefs.length = 1)) {
+  //       console.log(
+  //         `acteur ${json.uid} has mandat ${mandat.uid} ${mandat.xsiType} with organeRefs ${organesRefs}`,
+  //       )
+  //     }
+  //   })
+  // }
 }
 
 function readFilesInSubdir(subDir: string): string[] {
@@ -34,4 +42,14 @@ function readFileAsJson(filePath: string): any {
       encoding: 'utf8',
     }),
   )
+}
+
+function readAllDeputesJsonFromNosdeputes(): any[] {
+  return [13, 14, 15, 16].map(readDeputesJsonFromNosDeputes).flat()
+}
+
+function readDeputesJsonFromNosDeputes(legislature: number) {
+  return readFileAsJson(
+    `./other/nosdeputes_deputes${legislature}.json`,
+  ).deputes.map((_: any) => _.depute)
 }
